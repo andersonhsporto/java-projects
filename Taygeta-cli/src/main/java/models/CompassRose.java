1603cfd4bad7dc2b00a9bce4b2dfc1819045011d
andersonhsporto <anderson.higo2@gmail.com>
2022-08-31 20:29:15 -0300

    add: isValidDirection parseDirection

--- Taygeta-cli/src/main/java/models/CompassRose.java
+++ Taygeta-cli/src/main/java/models/CompassRose.java
@@ -8,4 +8,42 @@ public class CompassRose {
     EAST,
     WEST
   }
+
+  public static boolean isValidDirection(String command) {
+    String undercaseCommand = command.toLowerCase();
+
+    switch (undercaseCommand) {
+      case "north", "norte", "n", "south", "sul", "s" -> {
+        return true;
+      }
+      case "east", "leste", "e", "l", "west", "oeste", "w", "o" -> {
+        return true;
+      }
+      default -> {
+        return false;
+      }
+    }
+  }
+
+  public static Compass parseDirection(String command) {
+    String undercaseCommand = command.toLowerCase();
+
+    switch (undercaseCommand) {
+      case "north", "norte", "n" -> {
+        return Compass.NORTH;
+      }
+      case "south", "sul", "s" -> {
+        return Compass.SOUTH;
+      }
+      case "east", "leste", "e" -> {
+        return Compass.EAST;
+      }
+      case "west", "oeste", "w", "o" -> {
+        return Compass.WEST;
+      }
+      default -> {
+        return null;
+      }
+    }
+  }
 }
