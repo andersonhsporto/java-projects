1314e308237e471dd81e4a4c1665b215471c8d35
andersonhsporto <anderson.higo2@gmail.com>
2022-09-01 01:47:01 -0300

    fix: move tests to ValidationServiceTest

--- Taygeta-cli/src/test/java/command/MissionControlTest.java
+++ Taygeta-cli/src/test/java/command/MissionControlTest.java
@@ -28,50 +28,6 @@ class MissionControlTest {
     missionControl.addProbeToPlanet(new Probe(1, 0, 0, Compass.NORTH), 0);
     assertEquals(1, missionControl.getPlanets().size());
   }
-
-  @Test
-  @DisplayName("Plane exist by id true")
-  void truePlaneExistByid() {
-    MissionControl missionControl = new MissionControl();
-    missionControl.addPlanet("10x10");
-    assertTrue(ValidationService.planetExistsById("0", missionControl));
-  }
-
-  @Test
-  @DisplayName("Plane exist by id false")
-  void falsePlaneExistByid() {
-    MissionControl missionControl = new MissionControl();
-    missionControl.addPlanet("55x10");
-    assertFalse(ValidationService.planetExistsById("1", missionControl));
-  }
-
-  @Test
-  @DisplayName("Plane exist by id true multiple planets")
-  void falsePlaneExistByidMultipleTrue() {
-    MissionControl missionControl = new MissionControl();
-    missionControl.addPlanet("55x10");
-    missionControl.addPlanet("1x1");
-    assertTrue(ValidationService.planetExistsById("1", missionControl));
-  }
-
-  @Test
-  @DisplayName("Plane exist by id false multiple planets")
-  void falsePlaneExistByidMultipleFalse() {
-    MissionControl missionControl = new MissionControl();
-    missionControl.addPlanet("55x10");
-    missionControl.addPlanet("1x1");
-    assertFalse(ValidationService.planetExistsById("2", missionControl));
-  }
-
-  @Test
-  @DisplayName("Plane exist by negative id")
-  void falsePlanetExistNegativeId() {
-    MissionControl missionControl = new MissionControl();
-    missionControl.addPlanet("55x10");
-    missionControl.addPlanet("1x1");
-    assertFalse(ValidationService.planetExistsById("-1", missionControl));
-  }
-
   @Test
   @DisplayName("Size of planets list is 5")
   void sizeOfPlanetsListIs5() {
