6abd8b0cdb1fb5452da96a5f85d2e27aeb10e4b7
andersonhsporto <anderson.higo2@gmail.com>
2022-09-02 16:46:09 -0300

    add: error, success and defaultMessage

--- Taygeta-cli/src/main/java/command/Message.java
+++ Taygeta-cli/src/main/java/command/Message.java
@@ -2,8 +2,21 @@ package command;
 
 public class Message {
 
-  public static void greetings() {
+  public void greetings() {
     System.out.println(ColorWrapper.cyan("Welcome to Taygeta! CLI version"));
     System.out.println(ColorWrapper.cyan("The available commands are: add-planet, add-probe"));
   }
+
+  public void error(String message) {
+    System.out.println(ColorWrapper.red(message));
+  }
+
+  public void success(String message) {
+    System.out.println(ColorWrapper.green(message));
+  }
+
+  public void defaultMessage(String message) {
+    System.out.println(ColorWrapper.cyan(message));
+  }
+
 }
