e65a0eef88a2e9b088e11d0c38549a6c255e340a
andersonhsporto <anderson.higo2@gmail.com>
2022-09-02 21:13:13 -0300

    fix: removal commands sentence

--- Taygeta-cli/src/main/java/command/Message.java
+++ Taygeta-cli/src/main/java/command/Message.java
@@ -4,7 +4,6 @@ public class Message {
 
   public void greetings() {
     System.out.println(ColorWrapper.cyan("Welcome to Taygeta! CLI version"));
-    System.out.println(ColorWrapper.cyan("The available commands are: add-planet, add-probe"));
   }
 
   public void error(String message) {
