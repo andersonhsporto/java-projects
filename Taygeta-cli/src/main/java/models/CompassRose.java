6825fec47cdc4dc23ceae0a7e5e0fa4b4a31887f
andersonhsporto <anderson.higo2@gmail.com>
2022-09-04 14:49:22 -0300

    refactor: rename member functions

--- Taygeta-cli/src/main/java/models/CompassRose.java
+++ Taygeta-cli/src/main/java/models/CompassRose.java
@@ -2,20 +2,20 @@ package models;
 
 public class CompassRose {
 
-  public static boolean isValidDirection(String command) {
-    String underscoreCommand = command.toLowerCase();
+  public static boolean isValidCardinal(String command) {
+    String lowerCaseCommand = command.toLowerCase();
 
-    return switch (underscoreCommand) {
+    return switch (lowerCaseCommand) {
       case "north", "south", "east", "west", "norte", "sul", "leste", "oeste" -> true;
       case "n", "s", "e", "l", "w", "o" -> true;
       default -> false;
     };
   }
 
-  public static Cardinal parseDirection(String command) {
-    String underscoreCommand = command.toLowerCase();
+  public static Cardinal parseCardinal(String command) {
+    String lowerCaseCommand = command.toLowerCase();
 
-    return switch (underscoreCommand) {
+    return switch (lowerCaseCommand) {
       case "north", "norte", "n" -> Cardinal.NORTH;
       case "south", "sul", "s" -> Cardinal.SOUTH;
       case "east", "leste", "e", "l" -> Cardinal.EAST;
