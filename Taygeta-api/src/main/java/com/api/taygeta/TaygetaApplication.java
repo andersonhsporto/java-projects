2193a1ee62bb4bc8c4add1cba0651d4064be7305
andersonhsporto <anderson.higo2@gmail.com>
2022-09-10 01:10:43 -0300

    Initial commit

---
+++ Taygeta-api/src/main/java/com/api/taygeta/TaygetaApplication.java
@@ -0,0 +1,13 @@
+package com.api.taygeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaygetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaygetaApplication.class, args);
+	}
+
+}
