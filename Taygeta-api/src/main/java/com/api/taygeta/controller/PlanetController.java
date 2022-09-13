4b50d92e6b5ab5caa3450f59e454793d40d16612
andersonhsporto <anderson.higo2@gmail.com>
2022-09-13 02:27:50 -0300

    add: PlanetController.java

---
+++ Taygeta-api/src/main/java/com/api/taygeta/controller/PlanetController.java
@@ -0,0 +1,14 @@
+package com.api.taygeta.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/planets")
+public class PlanetController {
+
+
+
+}
