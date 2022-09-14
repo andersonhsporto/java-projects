4213f8d4f3098ac388f6d3db1885b1a2ec153b5a
andersonhsporto <anderson.higo2@gmail.com>
2022-09-14 01:19:35 -0300

    add: ProbeRepository

---
+++ Taygeta-api/src/main/java/com/api/taygeta/repositories/ProbeRepository.java
@@ -0,0 +1,8 @@
+package com.api.taygeta.repositories;
+
+import com.api.taygeta.entities.ProbeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProbeRepository extends JpaRepository<ProbeEntity, Long> {
+
+}
