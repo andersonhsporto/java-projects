471416183598eb4a3e1439cd5e455088b7a95c94
andersonhsporto <anderson.higo2@gmail.com>
2022-09-14 00:28:40 -0300

    add: injection of missionService

--- Taygeta-api/src/main/java/com/api/taygeta/controller/PlanetController.java
+++ Taygeta-api/src/main/java/com/api/taygeta/controller/PlanetController.java
@@ -1,6 +1,10 @@
 package com.api.taygeta.controller;
 
+import com.api.taygeta.services.MissionService;
+import com.api.taygeta.entities.PlanetEntity;
+import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/api/v1/planets")
 public class PlanetController {
+
+  private final MissionService missionService;
+
+  public PlanetController(MissionService missionService) {
+    this.missionService = missionService;
+  }
+
 
 
 
