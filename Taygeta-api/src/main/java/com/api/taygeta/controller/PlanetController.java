0e4f67a0321cf3ecde9f395c6e976bf33ee71d04
andersonhsporto <anderson.higo2@gmail.com>
2022-09-15 00:18:36 -0300

    add: rest constrollers

--- Taygeta-api/src/main/java/com/api/taygeta/controller/PlanetController.java
+++ Taygeta-api/src/main/java/com/api/taygeta/controller/PlanetController.java
@@ -1,12 +1,12 @@
 package com.api.taygeta.controller;
 
-import com.api.taygeta.entities.PlanetEntity;
 import com.api.taygeta.services.PlanetService;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,15 +22,23 @@ public class PlanetController {
   }
 
   @GetMapping()
-  public ResponseEntity<List<PlanetEntity>> getPlanets() {
+  public ResponseEntity<Object> getPlanets() {
     return planetService.getPlanets();
   }
 
-  @PostMapping()
-  public ResponseEntity<String> createPlanet() {
-    return planetService.makePlanet();
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getPlanetById(@PathVariable Long id) {
+    return planetService.getPlanetById(id);
   }
 
+  @PostMapping("/{string}")
+  public ResponseEntity<String> createPlanet(@PathVariable String string) {
+    return planetService.makePlanet(string);
+  }
 
+  @PutMapping("/{id}/{string}")
+  public ResponseEntity<String> updatePlanet(@PathVariable Long id, @PathVariable String string) {
+    return planetService.updatePlanet(id, string);
+  }
 
 }
