911799731c2004508bc5f176020f9f9b3414e5cc
andersonhsporto <anderson.higo2@gmail.com>
2022-09-16 23:34:07 -0300

    add: number of probes to PlanetDTO

--- Taygeta-api/src/main/java/com/api/taygeta/dto/PlanetDTO.java
+++ Taygeta-api/src/main/java/com/api/taygeta/dto/PlanetDTO.java
@@ -2,13 +2,19 @@ package com.api.taygeta.dto;
 
 import com.api.taygeta.entities.PlanetEntity;
 
-public record PlanetDTO(Long id, Integer width, Integer height, Integer area) {
+public record PlanetDTO(Long id,
+                        Integer width,
+                        Integer height,
+                        Integer area,
+                        Integer numberOfProbes) {
 
   public static PlanetDTO fromEntity(PlanetEntity planet) {
     return new PlanetDTO(
         planet.getId(),
         planet.getWidth(),
         planet.getHeight(),
-        planet.getArea());
+        planet.getArea(),
+        planet.getProbesCount());
   }
+
 }
