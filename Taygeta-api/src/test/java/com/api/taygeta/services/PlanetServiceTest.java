68fa62ab0c4a2ec2ad0255c7c04ac01545bf03c4
andersonhsporto <anderson.higo2@gmail.com>
2022-09-19 01:52:23 -0300

    add: unit test in PlanetRepository

---
+++ Taygeta-api/src/test/java/com/api/taygeta/services/PlanetServiceTest.java
@@ -0,0 +1,55 @@
+package com.api.taygeta.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//@DataJpaTest
+//class PlanetServiceTest {
+//
+//  @BeforeEach
+//  void setUp() {
+//  }
+//
+//  @Test
+//  void getAllPlanets() {
+//  }
+//
+//  @Test
+//  void getPlanetById() {
+//  }
+//
+//  @Test
+//  void getPlanetProbeById() {
+//  }
+//
+//  @Test
+//  void makePlanet() {
+//  }
+//
+//  @Test
+//  void updatePlanet() {
+//  }
+//
+//  @Test
+//  void updatePlanetEntity() {
+//  }
+//
+//  @Test
+//  void deletePlanet() {
+//  }
+//
+//  @Test
+//  void deletePlanetProbes() {
+//  }
+//
+//  @Test
+//  void deleteProbesList() {
+//  }
+//
+//  @Test
+//  void commandInPlanetSizeFormat() {
+//  }
+// }
