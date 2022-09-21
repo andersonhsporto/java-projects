c3b15d6dd913c0cec2c80517db1742ce9cef3c2d
andersonhsporto <anderson.higo2@gmail.com>
2022-09-21 02:09:59 -0300

    refactor: PlanetService tests

--- Taygeta-api/src/test/java/com/api/taygeta/services/PlanetServiceTest.java
+++ Taygeta-api/src/test/java/com/api/taygeta/services/PlanetServiceTest.java
@@ -1,55 +1,55 @@
 package com.api.taygeta.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//@DataJpaTest
-//class PlanetServiceTest {
-//
-//  @BeforeEach
-//  void setUp() {
-//  }
-//
-//  @Test
-//  void getAllPlanets() {
-//  }
-//
-//  @Test
-//  void getPlanetById() {
-//  }
-//
-//  @Test
-//  void getPlanetProbeById() {
-//  }
-//
-//  @Test
-//  void makePlanet() {
-//  }
-//
-//  @Test
-//  void updatePlanet() {
-//  }
-//
-//  @Test
-//  void updatePlanetEntity() {
-//  }
-//
-//  @Test
-//  void deletePlanet() {
-//  }
-//
-//  @Test
-//  void deletePlanetProbes() {
-//  }
-//
-//  @Test
-//  void deleteProbesList() {
-//  }
-//
-//  @Test
-//  void commandInPlanetSizeFormat() {
-//  }
-// }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+class PlanetServiceTest {
+
+  @Autowired
+  private PlanetService planetService;
+
+  @Test
+  @DisplayName("Planet string is invalid when it has less than 3 characters")
+  public void itShouldCheckIfPlanetStringIsInvalidWhenItHasLessThan3Characters() {
+    var planetString = "5x";
+    var isValid = planetService.commandInPlanetSizeFormat(planetString);
+
+    assertFalse(isValid);
+  }
+
+  @Test
+  @DisplayName("Planet string is invalid when x is a negative number")
+  public void itShouldCheckIfPlanetStringIsInvalidWhenXIsANegativeNumber() {
+    var planetString = "-5x5";
+    var isValid = planetService.commandInPlanetSizeFormat(planetString);
+
+    assertFalse(isValid);
+  }
+
+  @Test
+  @DisplayName("Planet string is invalid when y is a negative number")
+  public void itShouldCheckIfPlanetStringIsInvalidWhenYIsANegativeNumber() {
+    var planetString = "5x-5";
+    var isValid = planetService.commandInPlanetSizeFormat(planetString);
+
+    assertFalse(isValid);
+  }
+
+  @Test
+  @DisplayName("Planet string is invalid when x is not a number")
+  public void itShouldCheckIfPlanetStringIsInvalidWhenXIsNotANumber() {
+    var planetString = "x5";
+    var isValid = planetService.commandInPlanetSizeFormat(planetString);
+
+    assertFalse(isValid);
+  }
+}
