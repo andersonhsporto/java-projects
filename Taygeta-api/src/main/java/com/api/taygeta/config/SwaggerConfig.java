8ef8c4855efd5801d709da9f88c3783c56bf7801
andersonhsporto <anderson.higo2@gmail.com>
2022-09-23 17:34:38 -0300

    style: optimize imports

--- Taygeta-api/src/main/java/com/api/taygeta/config/SwaggerConfig.java
+++ Taygeta-api/src/main/java/com/api/taygeta/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import com.fasterxml.classmate.TypeResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -31,7 +34,24 @@ public class SwaggerConfig {
         .build()
         .additionalModels(
             typeResolver.resolve(PlanetDTO.class),
-            typeResolver.resolve(ProbeDTO.class));
+            typeResolver.resolve(ProbeDTO.class))
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("Taygeta API")
+        .description("API Rest implementation of the exploring mars problem")
+        .version("v1")
+        .contact(contact())
+        .build();
+  }
+
+  private Contact contact() {
+    return new Contact(
+        "Anderson Porto",
+        "https://www.linkedin.com/in/andersonhsporto/",
+        "anderson.higo2@gmail.com");
   }
 
 }
