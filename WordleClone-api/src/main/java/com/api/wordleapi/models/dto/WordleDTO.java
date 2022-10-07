310bb1bcfd1f8376a72d157bf79e7caec98bc729
andersonhsporto <anderson.higo2@gmail.com>
2022-10-07 02:37:10 -0300

    fix: dto

--- WordleClone-api/src/main/java/com/api/wordleapi/models/dto/WordleDTO.java
+++ WordleClone-api/src/main/java/com/api/wordleapi/models/dto/WordleDTO.java
@@ -1,4 +1,28 @@
 package com.api.wordleapi.models.dto;
 
-public record WordleDTO(String word) {
+public class WordleDTO {
+
+  private String word;
+
+  private String response;
+
+  public WordleDTO() {
+  }
+
+  public WordleDTO(String word, String response) {
+    this.word = word;
+    this.response = response;
+  }
+
+  public static WordleDTO of(String word, String response) {
+    return new WordleDTO(word, response);
+  }
+
+  public String getWord() {
+    return word;
+  }
+
+  public String getResponse() {
+    return response;
+  }
 }
