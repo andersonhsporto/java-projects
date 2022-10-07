463174d82711d3df0122d9dbc515730785a61b95
andersonhsporto <anderson.higo2@gmail.com>
2022-10-07 02:36:26 -0300

    add: csv bean

--- WordleClone-api/src/main/java/com/api/wordleapi/WordleApiApplication.java
+++ WordleClone-api/src/main/java/com/api/wordleapi/WordleApiApplication.java
@@ -1,13 +1,21 @@
 package com.api.wordleapi;
 
+import com.api.wordleapi.services.CsvReader;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WordleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WordleApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WordleApiApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner updateDatabase(CsvReader csvReader) {
+    return args -> csvReader.readCsv();
+  }
 
 }
