ab302c70844ece76479ec0bf1a0c5c5cdf860708
andersonhsporto <anderson.higo2@gmail.com>
2022-10-06 23:08:32 -0300

    add: add entity

--- WordleClone-api/src/main/java/com/api/wordleapi/models/entities/WordleEntity.java
+++ WordleClone-api/src/main/java/com/api/wordleapi/models/entities/WordleEntity.java
@@ -1,5 +1,41 @@
 package com.api.wordleapi.models.entities;
 
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class WordleEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String word;
+
+  public WordleEntity(String word) {
+    this.word = word;
+  }
+
+  public WordleEntity() {
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    WordleEntity that = (WordleEntity) o;
+    return Objects.equals(id, that.id) && Objects.equals(word, that.word);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, word);
+  }
 }
