9d868aff66e16d4a905ef9cd7fdf03b999fca7dd
andersonhsporto <anderson.higo2@gmail.com>
2022-10-07 02:37:39 -0300

    fix: crossOrigin and requestMapping

--- WordleClone-api/src/main/java/com/api/wordleapi/controller/WordleController.java
+++ WordleClone-api/src/main/java/com/api/wordleapi/controller/WordleController.java
@@ -1,15 +1,28 @@
 package com.api.wordleapi.controller;
 
+import com.api.wordleapi.models.dto.WordleDTO;
+import com.api.wordleapi.services.WordService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/wordle")
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/wordle")
 public class WordleController {
 
+  private final WordService wordService;
+
+  public WordleController(WordService wordService) {
+    this.wordService = wordService;
+  }
+
   @GetMapping
-  public ResponseEntity<?> getWordle() {
-    return ResponseEntity.ok("Hello World");
+  public ResponseEntity<?> getWordle(@RequestBody WordleDTO wordleDTO) {
+    return wordService.getWordle(wordleDTO);
   }
 
 }
