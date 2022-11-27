54670c6d58d8c495e6e5545ec4d00072ea1e28c3
andersonhsporto <anderson.higo2@gmail.com>
2022-11-27 02:28:42 -0300

    refactor: main controller configurations

--- Url-shortener/src/main/java/dev/anderson/javaurlshortener/controllers/UrlController.java
+++ Url-shortener/src/main/java/dev/anderson/javaurlshortener/controllers/UrlController.java
@@ -1,5 +1,26 @@
 package dev.anderson.javaurlshortener.controllers;
 
+import dev.anderson.javaurlshortener.Dtos.UrlDto;
+import dev.anderson.javaurlshortener.services.UrlService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/urls")
 public class UrlController {
+
+  private final UrlService urlService;
+
+  public UrlController(UrlService urlService) {
+    this.urlService = urlService;
+  }
+
+  @PostMapping
+  public String createUrl(@RequestBody UrlDto urlDto) {
+    return urlService.createUrl(urlDto);
+  }
+
 
 }
