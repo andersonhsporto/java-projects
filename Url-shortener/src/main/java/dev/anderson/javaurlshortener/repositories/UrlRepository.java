7e5f16752ca9a05a0120cdb190017d62b2c5db75
andersonhsporto <anderson.higo2@gmail.com>
2022-11-27 20:16:39 -0300

    add: findByShortUrl member function

--- Url-shortener/src/main/java/dev/anderson/javaurlshortener/repositories/UrlRepository.java
+++ Url-shortener/src/main/java/dev/anderson/javaurlshortener/repositories/UrlRepository.java
@@ -1,6 +1,7 @@
 package dev.anderson.javaurlshortener.repositories;
 
 import dev.anderson.javaurlshortener.entities.UrlEntity;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface UrlRepository extends MongoRepository<UrlEntity, String> {
 
   boolean existsByUrl(String url);
+
+  Optional<UrlEntity> findByShortUrl(String shortUrl);
 
 }
