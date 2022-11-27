ebe0102a26161d63174406f3f539025d0a8da9ea
andersonhsporto <anderson.higo2@gmail.com>
2022-11-27 20:29:33 -0300

    refactor: using constructor-based injection

--- Url-shortener/src/main/java/dev/anderson/javaurlshortener/services/Implementation/UrlServiceImpl.java
+++ Url-shortener/src/main/java/dev/anderson/javaurlshortener/services/Implementation/UrlServiceImpl.java
@@ -12,8 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UrlServiceImpl implements UrlService {
 
+  private final UrlRepository urlRepository;
+
   @Autowired
-  private UrlRepository urlRepository;
+  public UrlServiceImpl(UrlRepository urlRepository) {
+    this.urlRepository = urlRepository;
+  }
 
   @Override
   public String shortenUrl(UrlDto urlDto) {
