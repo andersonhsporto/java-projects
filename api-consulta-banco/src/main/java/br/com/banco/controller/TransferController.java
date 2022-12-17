e50c03829313f4990e3528f68fef41a381bdea14
andersonhsporto <anderson.higo2@gmail.com>
2022-12-17 00:31:23 -0300

    add: getByTransferId member functions

--- api-consulta-banco/src/main/java/br/com/banco/controller/TransferController.java
+++ api-consulta-banco/src/main/java/br/com/banco/controller/TransferController.java
@@ -1,14 +1,23 @@
 package br.com.banco.controller;
 
+import br.com.banco.service.TransferService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/transfers")
+@RequiredArgsConstructor
 public class TransferController {
 
-  private Long id;
+  private final TransferService transferService;
 
+  @GetMapping
+  public ResponseEntity<?> getByTransferId() {
+    return transferService.getByTransferId();
+  }
 
 
 }
