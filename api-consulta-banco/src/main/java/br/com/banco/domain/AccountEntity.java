17f090a09cf8fc18a2900678467198f138d1a425
andersonhsporto <anderson.higo2@gmail.com>
2022-12-16 23:17:42 -0300

    add: map using sql script

---
+++ api-consulta-banco/src/main/java/br/com/banco/domain/AccountEntity.java
@@ -0,0 +1,39 @@
+package br.com.banco.domain;
+
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "conta")
+public class AccountEntity {
+
+  @Id
+  @Column(name = "idConta", nullable = false, updatable = false)
+  @SequenceGenerator(
+      name = "primary_sequence",
+      sequenceName = "primary_sequence",
+      allocationSize = 1,
+      initialValue = 10000
+  )
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "primary_sequence"
+  )
+  private Long id;
+
+  @Column(name = "nomeResponsavel", nullable = false, length = 50)
+  private String name;
+
+  @OneToMany(mappedBy = "conta")
+  private Set<TransferEntity> contaTransferencias;
+
+}
---
+++ api-consulta-banco/src/main/java/br/com/banco/domain/TransferEntity.java
@@ -0,0 +1,51 @@
+package br.com.banco.domain;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "transferencia")
+public class TransferEntity {
+
+  @Id
+  @Column(nullable = false, updatable = false)
+  @SequenceGenerator(
+      name = "primary_sequence",
+      sequenceName = "primary_sequence",
+      allocationSize = 1,
+      initialValue = 10000
+  )
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "primary_sequence"
+  )
+  private Long id;
+
+  @Column(name = "dataTransferencia", nullable = false)
+  private OffsetDateTime transferDate;
+
+  @Column(name = "valor", nullable = false, precision = 22, scale = 2)
+  private BigDecimal value;
+
+  @Column(name = "tipo", nullable = false, length = 15)
+  private String type;
+
+  @Column(name = "nomeOperadorTransacao", length = 50)
+  private String operatorName;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "conta_id", nullable = false)
+  private AccountEntity conta;
+}
