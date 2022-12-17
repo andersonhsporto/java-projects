a63beddcb05444c11b084c55838b84e1abd96f06
andersonhsporto <anderson.higo2@gmail.com>
2022-12-17 00:31:35 -0300

    add: input transfer DTO

---
+++ api-consulta-banco/src/main/java/br/com/banco/DTO/InputTransferDTO.java
@@ -0,0 +1,15 @@
+package br.com.banco.DTO;
+
+import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class InputTransferDTO {
+
+  private OffsetDateTime initialDate;
+
+  private OffsetDateTime finalDate;
+
+}
