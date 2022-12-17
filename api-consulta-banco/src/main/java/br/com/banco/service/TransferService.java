25c1ac0a91756982abf8e0e8012b938e366e05e9
andersonhsporto <anderson.higo2@gmail.com>
2022-12-17 20:43:04 -0300

    add: persist failsafe member functions

--- api-consulta-banco/src/main/java/br/com/banco/service/TransferService.java
+++ api-consulta-banco/src/main/java/br/com/banco/service/TransferService.java
@@ -1,7 +1,7 @@
 package br.com.banco.service;
 
-import br.com.banco.DTO.InputTransferDTO;
-import br.com.banco.DTO.TransferDTO;
+import br.com.banco.domain.DTO.InputTransferDTO;
+import br.com.banco.domain.DTO.TransferDTO;
 import br.com.banco.domain.TransferEntity;
 import br.com.banco.repository.TransferRepository;
 import java.time.OffsetDateTime;
@@ -17,17 +17,43 @@ public class TransferService {
 
   private final TransferRepository transferRepository;
 
-  public ResponseEntity<?> getByTransferId() {
-    Optional<TransferEntity> entity = transferRepository.findById(1L);
+  public ResponseEntity<?> getByTransferId(InputTransferDTO inputTransferDTO) {
+    try {
+      return persistById(inputTransferDTO);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().build();
+    }
+  }
 
-    if (entity.isPresent()) {
-      return ResponseEntity.ok(TransferDTO.fromEntity(entity.get()));
+  public ResponseEntity<?> getTransferBetweenTwoDates(InputTransferDTO inputTransferDTO) {
+    try {
+      return persistBetweenTwoDates(inputTransferDTO);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
+  public ResponseEntity<?> getTransferByOperatorName(InputTransferDTO inputTransferDTO) {
+    try {
+      return persistByOperatorName(inputTransferDTO);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
+  private ResponseEntity<?> persistById(InputTransferDTO inputTransferDTO) throws Exception {
+    Optional<TransferEntity> transferEntity = transferRepository.findById(
+        inputTransferDTO.getTransferId());
+
+    if (transferEntity.isPresent()) {
+      return ResponseEntity.ok(TransferDTO.fromEntity(transferEntity.get()));
     } else {
       return ResponseEntity.notFound().build();
     }
   }
 
-  public ResponseEntity<?> getTransferBetweenTwoDates(InputTransferDTO inputTransferDTO) {
+  private ResponseEntity<?> persistBetweenTwoDates(InputTransferDTO inputTransferDTO)
+      throws Exception {
     OffsetDateTime initialDate = inputTransferDTO.getInitialDate();
     OffsetDateTime finalDate = inputTransferDTO.getFinalDate();
     List<TransferEntity> entity = transferRepository.findByTransferDateBetween(initialDate,
@@ -40,8 +66,13 @@ public class TransferService {
     }
   }
 
-  public ResponseEntity<?> getTransferByOperatorName(InputTransferDTO inputTransferDTO) {
+  private ResponseEntity<?> persistByOperatorName(InputTransferDTO inputTransferDTO)
+      throws Exception {
     String operatorName = inputTransferDTO.getOperatorName();
+    if (operatorName.isEmpty()) {
+      throw new Exception("Operator name is empty");
+    }
+
     List<TransferEntity> entity = transferRepository.findByOperatorName(operatorName);
 
     if (entity.isEmpty()) {
