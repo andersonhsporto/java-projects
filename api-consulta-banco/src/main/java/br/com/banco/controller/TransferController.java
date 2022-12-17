62ea7aed8ce6a0a5945cafd1240abcade942814d
andersonhsporto <anderson.higo2@gmail.com>
2022-12-17 02:15:03 -0300

    add: getTransferBetweenTwoDates member function

--- api-consulta-banco/src/main/java/br/com/banco/controller/TransferController.java
+++ api-consulta-banco/src/main/java/br/com/banco/controller/TransferController.java
@@ -1,9 +1,12 @@
 package br.com.banco.controller;
 
+import br.com.banco.DTO.InputTransferDTO;
 import br.com.banco.service.TransferService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,17 @@ public class TransferController {
 
   private final TransferService transferService;
 
+  @ApiOperation(value = "Return a list of all transfers using a given id")
   @GetMapping
   public ResponseEntity<?> getByTransferId() {
     return transferService.getByTransferId();
+  }
+
+  @ApiOperation(value = "Get all transfer between two dates")
+  @GetMapping("/between-dates")
+  public ResponseEntity<?> getTransferBetweenTwoDates(
+      @RequestBody InputTransferDTO inputTransferDTO) {
+    return transferService.getTransferBetweenTwoDates(inputTransferDTO);
   }
 
 
