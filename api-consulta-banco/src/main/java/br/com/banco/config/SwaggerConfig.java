9772ec24f5fe17b19fd23a519c2dea6743510906
andersonhsporto <anderson.higo2@gmail.com>
2022-12-18 03:07:10 -0300

    fix: hide error controller in swagger

--- api-consulta-banco/src/main/java/br/com/banco/config/SwaggerConfig.java
+++ api-consulta-banco/src/main/java/br/com/banco/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.classmate.TypeResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -25,7 +26,7 @@ public class SwaggerConfig {
     return new Docket(DocumentationType.SWAGGER_2)
         .useDefaultResponseMessages(false)
         .select()
-        .apis(RequestHandlerSelectors.any())
+        .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
         .paths(PathSelectors.any())
         .build()
         .additionalModels(
