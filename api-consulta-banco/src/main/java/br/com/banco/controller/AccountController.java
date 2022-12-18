5da4c35519720263585efd61cb1ae638ed6c76dc
andersonhsporto <anderson.higo2@gmail.com>
2022-12-18 01:43:23 -0300

    add: getAccountById member function

--- api-consulta-banco/src/main/java/br/com/banco/controller/AccountController.java
+++ api-consulta-banco/src/main/java/br/com/banco/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +21,11 @@ public class AccountController {
   public ResponseEntity<?> getAllAccounts() {
     return accountService.getAllAccount();
   }
+
+  @GetMapping("/number")
+  public ResponseEntity<?> getAccountById(@RequestParam(required = false) Long id) {
+    return accountService.getAccountById(id);
+  }
+
 
 }
