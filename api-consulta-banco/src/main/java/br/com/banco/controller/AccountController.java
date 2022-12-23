592760afda45c2d6aeb7fa31ae82d5d752d85b97
andersonhsporto <anderson.higo2@gmail.com>
2022-12-23 19:58:42 -0300

    refactor: using tree get member functions

--- api-consulta-banco/src/main/java/br/com/banco/controller/AccountController.java
+++ api-consulta-banco/src/main/java/br/com/banco/controller/AccountController.java
@@ -1,10 +1,12 @@
 package br.com.banco.controller;
 
+import br.com.banco.domain.DTO.InputTransferDTO;
 import br.com.banco.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +19,33 @@ public class AccountController {
 
   private final AccountService accountService;
 
-  @GetMapping
-  public ResponseEntity<?> getAllAccounts() {
-    return accountService.getAllAccount();
+  @GetMapping("/number")
+  public ResponseEntity<?> getAccountById(
+      @RequestParam(required = true) Long id,
+      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+      @RequestParam(value = "size", required = false, defaultValue = "4") Integer size
+  ) {
+    return accountService.getAccountById(id, page, size);
   }
 
-  @GetMapping("/number")
-  public ResponseEntity<?> getAccountById(@RequestParam(required = true) Long id) {
-    return accountService.getAccountById(id);
+  @GetMapping("/date")
+  public ResponseEntity<?> getAccountByDate(
+      @RequestParam(required = true) Long id,
+      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+      @RequestParam(value = "size", required = false, defaultValue = "4") Integer size,
+      @RequestBody InputTransferDTO inputTransferDTO
+  ) {
+    return accountService.getAccountByDate(id, page, size, inputTransferDTO);
+  }
+
+  @GetMapping("/name")
+  public ResponseEntity<?> getAccountByName(
+      @RequestParam(required = true) Long id,
+      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+      @RequestParam(value = "size", required = false, defaultValue = "4") Integer size,
+      @RequestBody InputTransferDTO inputTransferDTO
+  ) {
+    return accountService.getAccountByName(id, page, size, inputTransferDTO);
   }
 
 
