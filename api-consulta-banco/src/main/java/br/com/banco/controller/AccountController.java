7d42a43df8d8e759cf81bb5ed60b746fee59b74c
andersonhsporto <anderson.higo2@gmail.com>
2022-12-24 03:02:40 -0300

    refactor: using only getAccount for get

--- api-consulta-banco/src/main/java/br/com/banco/controller/AccountController.java
+++ api-consulta-banco/src/main/java/br/com/banco/controller/AccountController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,34 +18,28 @@ public class AccountController {
 
   private final AccountService accountService;
 
-  @GetMapping("/number")
-  public ResponseEntity<?> getAccountById(
-      @RequestParam(required = true) Long id,
-      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-      @RequestParam(value = "size", required = false, defaultValue = "4") Integer size
-  ) {
-    return accountService.getAccountById(id, page, size);
-  }
-
-  @GetMapping("/date")
-  public ResponseEntity<?> getAccountByDate(
-      @RequestParam(required = true) Long id,
+  @GetMapping
+  public ResponseEntity<?> getAccount(
+      @RequestParam(value = "id", required = true) Long id,
       @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
       @RequestParam(value = "size", required = false, defaultValue = "4") Integer size,
-      @RequestBody InputTransferDTO inputTransferDTO
+      @RequestParam(value = "initialDate", required = false, defaultValue = "1990-12-20T00:00:00Z") String initialDate,
+      @RequestParam(value = "finalDate", required = false, defaultValue = "1990-12-20T00:00:00Z") String finalDate,
+      @RequestParam(value = "operatorName", required = false, defaultValue = "null") String operatorName
   ) {
-    return accountService.getAccountByDate(id, page, size, inputTransferDTO);
+    try {
+      return accountService.getAccount(
+          InputTransferDTO.of(
+              id,
+              page,
+              size,
+              initialDate,
+              finalDate,
+              operatorName)
+      );
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().build();
+    }
   }
-
-  @GetMapping("/name")
-  public ResponseEntity<?> getAccountByName(
-      @RequestParam(required = true) Long id,
-      @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
-      @RequestParam(value = "size", required = false, defaultValue = "4") Integer size,
-      @RequestBody InputTransferDTO inputTransferDTO
-  ) {
-    return accountService.getAccountByName(id, page, size, inputTransferDTO);
-  }
-
 
 }
