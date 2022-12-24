adfe72b7eb19bc3adbc3f3499191047d27bcee90
andersonhsporto <anderson.higo2@gmail.com>
2022-12-24 03:02:19 -0300

    refactor: repository interface

--- api-consulta-banco/src/main/java/br/com/banco/repository/TransferRepository.java
+++ api-consulta-banco/src/main/java/br/com/banco/repository/TransferRepository.java
@@ -3,7 +3,6 @@ package br.com.banco.repository;
 import br.com.banco.domain.AccountEntity;
 import br.com.banco.domain.TransferEntity;
 import java.time.OffsetDateTime;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,17 +11,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransferRepository extends PagingAndSortingRepository<TransferEntity, Long> {
 
-  List<TransferEntity> findByTransferDateBetween(OffsetDateTime initialDate,
-      OffsetDateTime finalDate);
-
-  List<TransferEntity> findByOperatorName(String operatorName);
-
   Page<TransferEntity> findAllByConta(AccountEntity conta, Pageable pageable);
 
   Page<TransferEntity> findAllByContaAndTransferDateBetween(
       AccountEntity conta, Pageable pageable, OffsetDateTime initialDate,
-      OffsetDateTime finalDate);
+      OffsetDateTime finalDate
+  );
 
   Page<TransferEntity> findAllByContaAndOperatorName(
       AccountEntity conta, Pageable pageable, String transferName);
+
+  Page<TransferEntity> findAllByContaAndOperatorNameAndTransferDateBetween(
+      AccountEntity conta, Pageable pageable, String transferName,
+      OffsetDateTime initialDate, OffsetDateTime finalDate
+  );
 }
