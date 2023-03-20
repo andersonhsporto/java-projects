2e26c8428223d2cb478b0f9eed5fb9e26e67fa54
Anderson Porto <anderson.higo2@gmail.com>
2023-03-20 13:31:33 -0300

    add: test

--- subrepo1/ex02/lista-1/Questao1.java
+++ subrepo1/ex02/lista-1/Questao1.java
@@ -5,6 +5,7 @@ class Main {
         "Nome: Jose de Souza da Silva\n" +
             "Data de Nascimento: 31/07/1980\n" +
             "Naturalidade: Coxixola\n" +
+			
             "Nacionalidade: Brasileira\n" +
             "Matricula: 20011234-5\n");
 
